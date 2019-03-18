5fcb4fd46870e661e0e546482f49b5e080d558a2
JoshLee0915 <joshLee0915@gmail.com>
2019-03-17 21:14:01 -0600

    Updated config manager to deal with project configs

--- TattlerDIR/TattlerConfigManager.py
+++ TattlerDIR/TattlerConfigManager.py
@@ -5,6 +5,7 @@ import logging
 class ConfigManager:
     CONFIG_FILE = 'Tattler.cfg'
     config = {}
+    projects = None
 
     def __getitem__(self, section):
         return self.config[section]
@@ -19,19 +20,25 @@ class ConfigManager:
     def generate_config():
         config = configparser.ConfigParser()
 
+        config['General'] = {'Projects': []}
+
         config['Logging'] = {'Level': logging.NOTSET,
-                             'LogFile': 'Tattler.log'}
+                             'Log_File': 'Tattler.log'}
 
         config['Discord'] = {'Token': '',
-                             'ReportChannel': 'ANY'}
+                             'Report_Channel': 'ANY'}
 
-        config['Discord_Optional'] = {'AnnouncementChannel': '',
-                                      'DumpErrorsToGeneral': False}
-
-        config['IssueTracker'] = {}
+        config['Discord_Optional'] = {'Announcement_Channel': '',
+                                      'Dump_Errors_To_General': False}
 
         with open(ConfigManager.CONFIG_FILE, 'w') as configfile:
             config.write(configfile)
+
+    def get_projects(self):
+        if not self.projects:
+            self.projects = ConfigManager.translate_list(self.General['projects'])
+
+        return self.projects
 
     def load(self):
         config = configparser.ConfigParser()
@@ -43,3 +50,15 @@ class ConfigManager:
 
         for section, value in config.items():
             self.config[section] = value
+
+    @staticmethod
+    def is_list(value):
+        return isinstance(value, str) and value.startswith('[') and value.endswith(']')
+
+    @staticmethod
+    def translate_list(value):
+        if ConfigManager.is_list(value):
+            value = value[1:-1].split(',')
+            return [x.strip() for x in value]
+
+        return None
