21e44e460e74b6461b94ed32f840e907fc3014f7
JoshLee0915 <joshLee0915@gmail.com>
2019-03-17 21:15:35 -0600

    Updated the main file to now setup the Tracker Manager before starting the bot

--- Tattler.py
+++ Tattler.py
@@ -1,5 +1,10 @@
+import TattlerDIR
 from TattlerDIR import TattlerConfigManager
+from TattlerDIR.TattlerTrackerManager import TattlerTrackerManager
+from TattlerDIR.Trackers.YouTrackTracker import YouTrackTracker
+
 from discord.ext import commands
+
 
 bot = commands.Bot(command_prefix='!')
 config_manager = TattlerConfigManager.ConfigManager()
@@ -7,7 +12,7 @@ config_manager = TattlerConfigManager.ConfigManager()
 
 @bot.event
 async def on_ready():
-    if config_manager.Discord_Optional['announcementchannel'] != '':
+    if config_manager.Discord_Optional['announcement_channel'] != '':
         for server in bot.connection.servers:
             for channel in server.channels:
                 if channel.name == config_manager.Discord_Optional['announcementchannel']:
@@ -19,6 +24,13 @@ async def on_ready():
 
 if __name__ == '__main__':
     config_manager.load()
+    TattlerDIR.trackerManager = TattlerTrackerManager(config_manager)
+
+    TattlerDIR.trackerManager.register_issue_tracker('YouTrack', YouTrackTracker)
+
+    for project in config_manager.General['projects']:
+        TattlerDIR.trackerManager.register_project(project)
+
     bot.load_extension("TattlerDIR")
 
     bot.run(config_manager.Discord['token'])
