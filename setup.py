7d8ccde3601eb636db7902f73b7ca26132697ec2
JoshLee0915 <joshLee0915@gmail.com>
2019-04-18 20:05:01 -0600

    Updated setup.py to take in account the name change to the youtrack lib

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author='Josh Lee',
     author_email='',
     description='',
-    install_requires=['youtrack-rest-python-library'],
-    dependency_links=['git+https://github.com/JoshLee0915/youtrack-rest-python-library.git#egg=youtrack-rest-python-library']
+    install_requires=['YTClient'],
+    dependency_links=['git+https://github.com/JoshLee0915/youtrack-rest-python-library.git#egg=YTClient']
 )
